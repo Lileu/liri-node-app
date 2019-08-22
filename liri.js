82c86892aa38420d0828b668eda80a83169d993e
Lisa Leu <Lisal@netwealth.com.au>
2019-08-22 17:56:54 +1000

    added function to process bandsintown API retrieved data

--- liri.js
+++ liri.js
@@ -1,18 +1,36 @@
+// DEPENDENCIES
 require("dotenv").config();
 
+
 var keys = require("./keys.js");
-
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+
+var instruction = process.argv[2];
+var parameter = process.argv.splice(3).join(" ");
+
+// 1. concert-this
+if (instruction === "concert-this") {
+    axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp");
+    .then(function(response){
+        var results = response.data;
+        if (results.length === 0) {
+            console.log("It doesn't look like " + parameter + "is playing near you. Try another band!")
+        } else {
+            console.log("Upcoming gigs for: " + parameter);
+            for (var res of results) {
+                var eventDate = moment(result.datetime).format("MM/DD/YYYY")
+            }
+            
+
+    }
+
+}
 
 
+// 3. movie-this
 
-// concert-this
-
-
-// spotify-this-song
-
-
-// movie-this
-
-
-// do-what-it-says
+// 4. do-what-it-says
