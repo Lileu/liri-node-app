4591db416f0c7056921f77110d8160b22f278c34
Lisa Leu <Lisal@netwealth.com.au>
2019-08-23 17:15:01 +1000

    added switch/case statements

--- liri.js
+++ liri.js
@@ -8,11 +8,43 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
 
-var instruction = process.argv[2];
+const instruction = process.argv[2];
 var parameter = process.argv.splice(3).join(" ");
 
+// -------------------------
+
+switch (instruction) {
+  case "concert-this":
+    if (parameter === "") {
+      console.log("Please enter a band you wanna see!");
+    } else {
+      concert(parameter);
+    }
+    break;
+  case "spotify-this-song":
+    if (parameter === "") {
+      spotifyThis("The Sign");
+      //q=name:abacab&type=album,track
+    } else {
+      spotifyThis(parameter);
+    }
+    break;
+  case "movie-this":
+    if (parameter === "") {
+      movieThis("Mr Nobody");
+    } else {
+      movieThis(parameter);
+    }
+    break;
+  default:
+    console.log(
+      "Invalid command. You can use any of the following commands: concert-this spotify-this-song movie-this or do-what-it-says"
+    );
+    break;
+}
+
 // 1. node liri.js concert-this <artist/band name here>
-if (instruction === "concert-this") {
+function concert(parameter) {
   axios
     .get(
       "https://rest.bandsintown.com/artists/" +
@@ -47,31 +79,38 @@ if (instruction === "concert-this") {
 }
 
 // 2. node liri.js spotify-this-song '<song name here>'
+function spotifyThis(parameter) {
+  spotify.search(
+    {
+      type: "track",
+      query: parameter
+    },
+    function(err, data) {
+      if (err) {
+        return console.log("Error Occured: " + err);
+      } else {
+        for (i = 0; i < 3; i++) {
+          var artistName = data.tracks.items[i].artists[0].name;
+          var songName = data.tracks.items[i].name;
+          var url = data.tracks.items[i].preview_url;
+          var albumName = data.tracks.items[i].album.name;
+
+          console.log("-------------------------");
+          console.log("Artist: " + artistName);
+          console.log("Song: " + songName);
+          console.log("Preview: " + url);
+          console.log("Album: " + albumName);
+          console.log("-------------------------");
+        }
+      }
+    }
+  );
+}
 
 //   console.log(response.data);
 
 // 3. node liri.js movie-this '<movie name here>'
-
-if (instruction === "movie-this" && parameter === "") {
-  axios
-    .get("http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy&")
-    .then(function(response) {
-      console.log("");
-      console.log("You should check out Mr. Nobody on Netflix!");
-      console.log("Title: " + response.data.Title);
-      console.log("Released: " + response.data.Year);
-      console.log("IMDB Rating: " + response.data.imdbRating);
-      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].value);
-      console.log("Produced in: " + response.data.Country);
-      console.log("Language: " + response.data.Language);
-      console.log("Plot: " + response.data.Actors);
-      console.log("--------------------------------------------");
-    })
-    .catch(function(err) {
-      if (err.response) {
-      }
-    });
-} else if (instruction === "movie-this") {
+function movieThis(parameter) {
   axios
     .get(
       "http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy"
@@ -94,3 +133,12 @@ if (instruction === "movie-this" && parameter === "") {
 }
 
 // 4. node liri.js do-what-it-says
+function doThing() {
+  fs.readFile("random.txt", "utf-8", function(err, data) {
+    if (err) {
+      return console.log(error);
+    }
+    var random = data.split(",");
+    spotifyThis(random[1]);
+  });
+}
