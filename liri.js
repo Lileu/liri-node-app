bc25fe2df3ac9f913ee6812729360625e2fbdd2f
Lisa Leu <Lisal@netwealth.com.au>
2019-08-23 12:41:35 +1000

    movie-this is working

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ var moment = require("moment");
 var instruction = process.argv[2];
 var parameter = process.argv.splice(3).join(" ");
 
-// 1. concert-this
+// 1. node liri.js concert-this <artist/band name here>
 if (instruction === "concert-this") {
   axios
     .get(
@@ -46,6 +46,51 @@ if (instruction === "concert-this") {
     });
 }
 
-// 3. movie-this
+// 2. node liri.js spotify-this-song '<song name here>'
 
-// 4. do-what-it-says
+//   console.log(response.data);
+
+// 3. node liri.js movie-this '<movie name here>'
+
+if (instruction === "movie-this" && parameter === "") {
+  axios
+    .get("http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy&")
+    .then(function(response) {
+      console.log("");
+      console.log("You should check out Mr. Nobody on Netflix!");
+      console.log("Title: " + response.data.Title);
+      console.log("Released: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].value);
+      console.log("Produced in: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Actors);
+      console.log("--------------------------------------------");
+    })
+    .catch(function(err) {
+      if (err.response) {
+      }
+    });
+} else if (instruction === "movie-this") {
+  axios
+    .get(
+      "http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy"
+    )
+    .then(function(response) {
+      console.log("");
+      console.log("Title: " + response.data.Title);
+      console.log("Released: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].value);
+      console.log("Produced in: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Actors);
+      console.log("--------------------------------------------");
+    })
+    .catch(function(err) {
+      if (err.response) {
+      }
+    });
+}
+
+// 4. node liri.js do-what-it-says
